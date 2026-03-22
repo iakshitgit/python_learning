c3809fd6e44e4002615bdb62fd9401fe697844fa
Akshit <akshit.rastogi27@gmail.com>
2026-03-22 17:22:26 +0530

    printDataType.py added

---
+++ printDataType.py
@@ -0,0 +1,8 @@
+from loguru import logger
+
+length_of_land = 100
+brick_cost_per_price = 10.5
+labour_mistri1 = "Jagmohan"
+is_home = True
+
+logger.info(f"Length of land: {length_of_land}, Bricks_cost_per_price: {brick_cost_per_price}, Labour_mistri1:{labour_mistri1}, is_home:{is_home}")
